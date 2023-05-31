1deacf14e196cb80b90157358ea3eb3f05cdc343
liam-engle <liamengle700@gmail.com>
2023-05-31 14:39:30 -0700

    form now requests data from $target

--- webscraperTk/__init__.py
+++ webscraperTk/__init__.py
@@ -1,29 +1,37 @@
 import tkinter as tk
-
+import requests
 def submit_form():
     target = target_entry.get()
     interval = int(interval_entry.get())
     print(f"Target: {target}")
     print(f"Interval: {interval}")
 
+    # Make the GET request using requests library
+    response = requests.get(target)
+    print(f"Response: {response.text}")
+
 # Create the main window
 window = tk.Tk()
 window.title("Form")
 
+# Create a frame
+frame = tk.Frame(window)
+frame.pack()
+
 # Create the target entry
-target_label = tk.Label(window, text="Target:")
+target_label = tk.Label(frame, text="Target:")
 target_label.pack()
-target_entry = tk.Entry(window)
+target_entry = tk.Entry(frame)
 target_entry.pack()
 
 # Create the interval entry
-interval_label = tk.Label(window, text="Interval:")
+interval_label = tk.Label(frame, text="Interval:")
 interval_label.pack()
-interval_entry = tk.Entry(window)
+interval_entry = tk.Entry(frame)
 interval_entry.pack()
 
 # Create the submit button
-submit_button = tk.Button(window, text="Submit", command=submit_form)
+submit_button = tk.Button(frame, text="Submit", command=submit_form)
 submit_button.pack()
 
 # Run the main loop
